9a893e3ae55009855ace5f9bf7cc748d4ca6b9e9
Fred Donega <fred.wejam@gmail.com>
2025-03-06 09:36:19 -0300

    ga

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "@/routes";
+import ReactGA from "react-ga4";
 import "./index.css";
+
+const analyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+
+// Inicializa o Google Analytics se o ID estiver definido
+if (analyticsId) {
+  ReactGA.initialize(analyticsId);
+}
 
 const queryClient = new QueryClient();
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Routes as DomRoutes, Route } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { Project } from "@/pages/Project";
+import { TrackPageView } from "./TrackPageView";
 export const Routes = () => {
   return (
     <DomRoutes>
+      <TrackPageView />
       <Route path="/" element={<Home />} />
       <Route path="/project/:projectId" element={<Project />} />
     </DomRoutes>
