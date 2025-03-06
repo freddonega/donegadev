36e500e3662051c747c1823354f9a3335f8e8081
Fred Donega <fred.wejam@gmail.com>
2025-03-06 09:46:07 -0300

    ga

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { Routes as DomRoutes, Route } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { Project } from "@/pages/Project";
-import { TrackPageView } from "./TrackPageView";
 export const Routes = () => {
   return (
     <DomRoutes>
       <Route path="/" element={<Home />} />
       <Route path="/project/:projectId" element={<Project />} />
-    </DomRoutes>
   );
 };
